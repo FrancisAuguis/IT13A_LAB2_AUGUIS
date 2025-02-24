2c0a537ea47afd4c6605d44bcfeaf96b9a963c00
MAKIENIPART <delacruzfrancis502@gmail.com>
2025-02-24 03:53:06 -0800

    added
    java

---
+++ Auguis_lab2/src/prelim/auguis_lab2_activities.java
@@ -0,0 +1,36 @@
+
+package prelim;
+
+import java.util.Scanner;
+
+
+public class auguis_lab2_activities {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+      
+        System.out.print("Enter first number: ");
+        int cat1 = scanner.nextInt();
+        
+        System.out.print("Enter second number: ");
+        int dog2 = scanner.nextInt();
+        
+        System.out.print("Enter third number: ");
+        int milo3 = scanner.nextInt();
+        
+       
+        int greatest = Math.max(cat1, Math.max(dog2, milo3));
+        
+        
+        System.out.println("The greatest number is: " + greatest);
+        
+        scanner.close();
+    }
+}
+
+
+
+
+
+
